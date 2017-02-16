cc92462d9a02e239070ce12861a63fd89f90669b
shamikumar <shamik@smartdatainc.ney>
2017-02-16 12:59:32 +0530

    Initial Commit

---
+++ FantasticView112UITests/FantasticView112UITests.swift
@@ -0,0 +1,36 @@
+//
+//  FantasticView112UITests.swift
+//  FantasticView112UITests
+//
+//  Created by Shami Kumar on 16/02/17.
+//  Copyright © 2017 Shami Kumar. All rights reserved.
+//
+
+import XCTest
+
+class FantasticView112UITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
